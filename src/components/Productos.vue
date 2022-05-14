1e1d1bff740fde4622d6d8472e08351f0fb457f9
Daniel <adrecolin@gmail.com>
2022-05-14 18:10:23 -0400

    buscador funcionando
    buscador que filtra al escribir una tecla,con algunos comentarios del buscador de pokemon y el ultimo metodo con el comentario y la fuente de donde salio todo

--- src/components/Productos.vue
+++ src/components/Productos.vue
@@ -1,24 +1,30 @@
 <template>
-  <div class="container mb-5">
-    <h2 class="text-center mb-3">Productos</h2>
-    <div class="row justify-content-center">
-      <div
-        class="card col-6 col-sm-2 m-3 p-0"
-        v-for="(item, index) in productos"
-        :key="index"
-      >
-        <div class="card-body">
-          <h5 class="card-title">{{ item.id }} {{ item.nombre }}</h5>
 
-          <p>{{ item.precio }}</p>
-          <img
-            class="card-img-top"
-            v-bind:src="item.image"
-            v-bind:alt="item.name"
-          />
-          <!--p class="card-text">Origen: {{ item.origin.name }}</p-->
-        </div>
-        <button type="button" class="btn btn-primary btn-sm">
+<!-- BUSCADOR SACADO DE LA PAGINA DE POKEMONES QUE SE VEIA GASTON ASI QUE SI DICE ALGO DE POKEMONES EN LOS COMENTARIOS, ES POR ESO -->
+   <div class="container cont">
+    <div class="row">
+      <div class="col-md-12">     
+            <h2 class="text-center mb-3">Productos en venta</h2>
+ 
+        <input type="text" v-model="buscar" class="form-control" placeholder="Buscar"/>              
+
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col-md-4" v-for="item in items" v-bind:key="item.id"> 
+        <div class="card mb-3">
+          <!-- METODO NUEVO QUE MUESTRA LA IMAGEN, LE HACE UN METODO Y UN REQUIRE EN LA LINEA 80 -->
+          <img class="card-img-top" :src="imagenProdu(item.img)" :alt="item.nombre"/>
+
+          <!-- METODO ANTIGUO QUE USABAMOS Y QUE NO AGARRABA LAS IMAGENES -->
+          <!-- <img class="card-img-top" :src="'/img' + item.img" v-bind:alt="item.nombre"/> -->
+
+          <div class="card-body">
+            <h3 class="card-title mb-3">{{ item.nombre }}</h3>
+            <p class="card-text"><strong>Precio:</strong> {{ item.precio }}</p>
+          </div>
+          <button type="button" class="btn btn-primary btn-sm">
           Agregar Favorito
         </button>
         <button
@@ -29,55 +35,57 @@
         >
           Descripción
         </button>
+        </div>
+
       </div>
+
     </div>
-  </div>
+
+   </div>
+
+
+
 </template>
 
 
 <script>
+
+
+import datos from "../assets/json/productos.json";
+
 export default {
+
+  // Exporto mi componente 'Pokemones' 
+  name: 'Productos',
+  props: {
+    msg: String
+  },
+  
+  // declaro el elemento buscar 
   data() {
     return {
-      productos: [
-        {
-          id: 1,
-          nombre: "Producto1",
-          image: require('../assets/imagen1.jpg'),
-          precio: 1000,
-        },
-        {
-          id: 2,
-          nombre: "Producto2",
-          image: require('../assets/imagen2.jpg'),
-          precio: 2000,
-        },
-        {
-          id: 3,
-          nombre: "Producto3",
-          image: require('../assets/imagen3.jpg'),
-          precio: 3000,
-        },
-        {
-          id: 4,
-          nombre: "Producto4",
-          image: require('../assets/imagen4.jpg'),
-          precio: 4000,
-        },
-        {
-          id: 5,
-          nombre: "Producto5",
-          image: require('../assets/imagen5.jpg'),
-          precio: 3000,
-        },
-        {
-          id: 6,
-          nombre: "Producto6",
-          image: require('../assets/imagen6.jpg'),
-          precio: 3000,
-        },
-      ],
-    };
+      buscar: ''
+    }
   },
-};
+    
+  // Creo un método llamado 'items' y obtengo los datos de los pokemones
+  // Asimismo filtro la búsqueda de los pokemones con el método filter()  
+  computed: {
+    items() {
+      return datos.filter(item => {
+        return item.nombre.toLowerCase().includes(this.buscar.toLowerCase());
+      });
+    },
+  },
+  // METODO SACADO DE https://medium.com/@dmitrymind/how-to-keep-array-data-inside-local-json-file-for-vue-app-46bb29d4ac53
+  methods: {
+    imagenProdu: function(img) {
+      return require("@/" + img);
+    },
+  },
+  
+}
+
+
+
 </script>
--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,20 +1,25 @@
 <template>
-  <h1>Inicio</h1>
   <Productos/>
+
   <router-view />
-  <!-- Campo de busqueda productos -->
-  <input type="text" v-model="buscar" class="form-control" placeholder="Ejemplo: Charmander"/>
 </template>
 
 <script>
 import Productos from "@/components/Productos.vue";
+
 export default {
-  components :{
-    Productos,
-  }
-}
+  components: {
+   Productos,
+   
+  },
+};
+
+
+
 </script>
 
 <style>
 
 </style>
+
+
